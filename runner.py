bece8302cc5632eddfe6dfc04d36615f0cb67120
Omer Akram <om26er@gmail.com>
2017-07-15 22:48:54 +0500

    Better config handling

--- runner.py
+++ runner.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 
+import configparser
+import os
+
 from autobahn.twisted.wamp import ApplicationRunner
 
 from pigpio import component as gpio
 
 
 if __name__ == '__main__':
-    runner = ApplicationRunner(url="ws://192.168.1.3:8080/ws", realm="realm1")
+    snap_data = os.environ.get('SNAP_DATA', None)
+    if snap_data:
+        config = configparser.ConfigParser()
+        config.read(os.path.join(snap_data, 'config'))
+        runner = ApplicationRunner(url=config.get('config', 'crossbar'), realm=config.get('config', 'realm'))
+    else:
+        runner = ApplicationRunner(url='ws://localhost:8080/ws', realm='realm1')
     runner.run(gpio.ClientSession, auto_reconnect=True)
