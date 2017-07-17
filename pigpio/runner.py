7e57c039f44a8d6fb7b39a61d77032d830b63b09
Omer Akram <om26er@gmail.com>
2017-07-18 00:10:28 +0500

    Cleanup runner, update config file name

--- pigpio/runner.py
+++ pigpio/runner.py
@@ -7,15 +7,20 @@ from autobahn.twisted.wamp import ApplicationRunner
 
 from pigpio import component as gpio
 
+NAME_CONF_FILE = '.pigpio.conf'
+DIR_SNAP_COMMON = 'SNAP_COMMON'
+
+
+def get_config_path():
+    if DIR_SNAP_COMMON in os.environ:
+        return os.path.join(os.environ[DIR_SNAP_COMMON], NAME_CONF_FILE)
+    return os.path.join(os.path.expanduser('~'), NAME_CONF_FILE)
+
 
 def main():
-    snap_data = os.environ.get('SNAP_DATA', None)
-    if snap_data:
-        config = configparser.ConfigParser()
-        config.read(os.path.join(snap_data, 'config'))
-        runner = ApplicationRunner(url=config.get('config', 'crossbar'), realm=config.get('config', 'realm'))
-    else:
-        runner = ApplicationRunner(url='ws://localhost:8080/ws', realm='realm1')
+    config = configparser.ConfigParser()
+    config.read(get_config_path())
+    runner = ApplicationRunner(url=config.get('config', 'crossbar'), realm=config.get('config', 'realm'))
     runner.run(gpio.ClientSession, auto_reconnect=True)
 
 
