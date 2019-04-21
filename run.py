f7b006f66c7bfb6a69ecf40c8ca423ac59727154
Omer Akram <om26er@gmail.com>
2019-04-22 03:32:30 +0500

    Rewrite to be a WAMP component only

--- run.py
+++ run.py
@@ -1,45 +1,75 @@
-#
-# Copyright (C) 2018  Omer Akram
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#
+#!/usr/bin/python3
 
 import os
 
-from crossbar import run
+from autobahn.twisted.component import Component, run
+from twisted.internet.endpoints import UNIXClientEndpoint
+from twisted.internet import reactor
+
+BASE_GPIO = '/sys/class/gpio'
+PATH_GPIO = os.path.join(BASE_GPIO, 'gpio{}')
 
 
-def _is_snap():
-    return os.environ.get('SNAP_NAME') == 'pigpio'
+def _turn(pin_number, value):
+    path = PATH_GPIO.format(pin_number)
+    if os.path.exists(path):
+        with open(os.path.join(path, 'direction'), 'w') as file:
+            file.write('out')
+        with open(os.path.join(path, 'value'), 'w') as file:
+            file.write(str(value))
+    else:
+        print("Path {} does not exist".format(path))
 
 
-def _get_cbdir():
-    return os.environ.get('SNAP_USER_DATA') if _is_snap() else 'crossbar-config'
+def turn_on(pin_number):
+    _turn(pin_number, 0)
 
 
-def get_start_params():
-    params = ['start', '--cbdir', _get_cbdir()]
-    if _is_snap():
-        params.append('--config')
-        params.append(os.path.join(os.environ.get('SNAP'), 'crossbar-config/config.json'))
-    return params
+def turn_off(pin_number):
+    _turn(pin_number, 1)
 
 
-def main():
-    run(get_start_params())
+def get_state(pin_number):
+    path = PATH_GPIO.format(pin_number)
+    with open(os.path.join(path, 'direction')) as file:
+        direction = file.read().strip()
+    with open(os.path.join(path, 'value')) as file:
+        value = file.read().strip()
+
+    return {
+        'pin_number': pin_number,
+        'direction': direction,
+        'value': value,
+        'value_verbose': 'on' if value == '0' else 'off'
+    }
+
+
+def get_states():
+    def is_gpio(item):
+        return item.startswith('gpio') and 'chip' not in item
+
+    return [get_state(int(pin.replace('gpio', ''))) for pin in filter(is_gpio, os.listdir(BASE_GPIO))]
 
 
 if __name__ == '__main__':
-    main()
+    if os.environ.get("SNAP_NAME") != "pigpio":
+        os.environ['SNAP_COMMON'] = os.path.expandvars('$HOME')
+
+    transport = {
+        "type": "rawsocket",
+        "url": "ws://localhost/ws",
+        "endpoint": UNIXClientEndpoint(reactor, os.path.join(os.environ['SNAP_COMMON'], 'deskconn.sock'))
+    }
+
+    component = Component(transports=[transport], realm="deskconn")
+
+
+    @component.on_join
+    def join(session, details):
+        print("Joined session....")
+        session.register(turn_on, "org.deskconn.gpio.turn_on")
+        session.register(turn_off, "org.deskconn.gpio.turn_off")
+        session.register(get_state, "org.deskconn.gpio.get_state")
+        session.register(get_states, "org.deskconn.gpio.get_states")
+
+    run([component])
