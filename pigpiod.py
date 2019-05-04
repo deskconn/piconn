b3eca410ba5537c35651e4d539ac06404b682027
Omer Akram <om26er@gmail.com>
2019-05-05 02:18:19 +0500

    Move the GPIO code into a central place

--- pigpiod.py
+++ pigpiod.py
@@ -4,49 +4,7 @@ from autobahn.twisted.component import Component, run
 from twisted.internet.endpoints import UNIXClientEndpoint
 from twisted.internet import reactor
 
-BASE_GPIO = '/sys/class/gpio'
-PATH_GPIO = os.path.join(BASE_GPIO, 'gpio{}')
-
-
-def _turn(pin_number, value):
-    path = PATH_GPIO.format(pin_number)
-    if os.path.exists(path):
-        with open(os.path.join(path, 'direction'), 'w') as file:
-            file.write('out')
-        with open(os.path.join(path, 'value'), 'w') as file:
-            file.write(str(value))
-    else:
-        print("Path {} does not exist".format(path))
-
-
-def turn_on(pin_number):
-    _turn(pin_number, 0)
-
-
-def turn_off(pin_number):
-    _turn(pin_number, 1)
-
-
-def get_state(pin_number):
-    path = PATH_GPIO.format(pin_number)
-    with open(os.path.join(path, 'direction')) as file:
-        direction = file.read().strip()
-    with open(os.path.join(path, 'value')) as file:
-        value = file.read().strip()
-
-    return {
-        'pin_number': pin_number,
-        'direction': direction,
-        'value': value,
-        'value_verbose': 'on' if value == '0' else 'off'
-    }
-
-
-def get_states():
-    def is_gpio(item):
-        return item.startswith('gpio') and 'chip' not in item
-
-    return [get_state(int(pin.replace('gpio', ''))) for pin in filter(is_gpio, os.listdir(BASE_GPIO))]
+import pygpio
 
 
 if __name__ == '__main__':
@@ -65,9 +23,9 @@ if __name__ == '__main__':
     @component.on_join
     def join(session, _):
         print("Joined session....")
-        session.register(turn_on, "org.deskconn.gpio.turn_on")
-        session.register(turn_off, "org.deskconn.gpio.turn_off")
-        session.register(get_state, "org.deskconn.gpio.get_state")
-        session.register(get_states, "org.deskconn.gpio.get_states")
+        session.register(pygpio.turn_on, "org.deskconn.gpio.turn_on")
+        session.register(pygpio.turn_off, "org.deskconn.gpio.turn_off")
+        session.register(pygpio.get_state, "org.deskconn.gpio.get_state")
+        session.register(pygpio.get_states, "org.deskconn.gpio.get_states")
 
     run([component])
