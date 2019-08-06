8d70251d3e25b80155b25210af72bd03bf93fef4
Omer Akram <om26er@gmail.com>
2019-08-07 02:30:30 +0500

    ensure the desconnd sock exists before connecting

--- pigpio.py
+++ pigpio.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-import os
 import argparse
+import os
+import sys
 
 from autobahn.twisted.component import Component, run
 from twisted.internet.endpoints import UNIXClientEndpoint
@@ -17,12 +18,15 @@ if __name__ == '__main__':
     if os.environ.get("SNAP_NAME") != "gpiod":
         os.environ['SNAP_COMMON'] = os.path.expandvars('$HOME')
 
+    sock_path = os.path.join(os.path.expandvars('$SNAP_COMMON/deskconnd-sock-dir'), 'deskconnd.sock')
+    if not os.path.exists(sock_path):
+        print("deskconnd not found, please make sure its installed and running.")
+        sys.exit(1)
+
     transport = {
         "type": "rawsocket",
         "url": "ws://localhost/ws",
-        "endpoint": UNIXClientEndpoint(reactor,
-                                       os.path.join(os.path.expandvars('$SNAP_COMMON/deskconnd-sock-dir'),
-                                                    'deskconnd.sock')),
+        "endpoint": UNIXClientEndpoint(reactor, sock_path),
         "serializer": "cbor",
     }
 
--- pigpiod.py
+++ pigpiod.py
@@ -1,34 +1,53 @@
 import os
+import time
 
 from autobahn.twisted.component import Component, run
 from twisted.internet.endpoints import UNIXClientEndpoint
 from twisted.internet import reactor
+import txaio
 
 import pygpio
+
+txaio.use_twisted()
+log = txaio.make_logger()
+
+
+def assemble(sock, reactor):
+    transport = {
+        "type": "rawsocket",
+        "url": "ws://localhost/ws",
+        "endpoint": UNIXClientEndpoint(reactor, sock),
+        "serializer": "cbor",
+    }
+    component = Component(transports=[transport], realm="deskconn")
+    component._transports[0].max_retries = 0
+
+    @component.on_join
+    def join(session, _):
+        log.info("connected to deskconnd")
+        session.register(pygpio.turn_on, "org.deskconn.gpio.turn_on")
+        session.register(pygpio.turn_off, "org.deskconn.gpio.turn_off")
+        session.register(pygpio.get_state, "org.deskconn.gpio.get_state")
+        session.register(pygpio.get_states, "org.deskconn.gpio.get_states")
+
+    @component.on_leave
+    def left(session, _):
+        log.info("disconnected from deskconnd")
+        enter_loop()
+    return component
+
+
+def enter_loop():
+    while not os.path.exists(sock_path):
+        time.sleep(1)
+        log.debug("deskconnd not found, please make sure its installed and running.")
+
+    run([assemble(sock_path, reactor)])
 
 
 if __name__ == '__main__':
     if os.environ.get("SNAP_NAME") != "gpiod":
         os.environ['SNAP_COMMON'] = os.path.expandvars('$HOME')
 
-    transport = {
-        "type": "rawsocket",
-        "url": "ws://localhost/ws",
-        "endpoint": UNIXClientEndpoint(reactor,
-                                       os.path.join(os.path.expandvars('$SNAP_COMMON/deskconnd-sock-dir'),
-                                                    'deskconnd.sock')),
-        "serializer": "cbor",
-    }
-
-    component = Component(transports=[transport], realm="deskconn")
-
-
-    @component.on_join
-    def join(session, _):
-        print("Joined session....")
-        session.register(pygpio.turn_on, "org.deskconn.gpio.turn_on")
-        session.register(pygpio.turn_off, "org.deskconn.gpio.turn_off")
-        session.register(pygpio.get_state, "org.deskconn.gpio.get_state")
-        session.register(pygpio.get_states, "org.deskconn.gpio.get_states")
-
-    run([component])
+    sock_path = os.path.join(os.path.expandvars('$SNAP_COMMON/deskconnd-sock-dir'), 'deskconnd.sock')
+    enter_loop()
